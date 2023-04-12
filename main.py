996bf882bb3df93f2aa5bf47aab9473b3063d8c2
aHarry01 <67612176+aHarry01@users.noreply.github.com>
2023-04-11 23:06:12 -0400

    Updated to get data from soil moisture sensor too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import smbus
+import spidev
 import DHT
 import pigpio
 
@@ -7,18 +8,26 @@ import pigpio
 DHT_PIN = 18 # GPIO18, pin12
 
 # I2C light intensity variables
-I2C_CHANNEL = 1 # I2C channe l1 is connected to GPIO pins
+I2C_CHANNEL = 1 # I2C channel1 is connected to GPIO pins
 I2C_ADDRESS = 0x23 # I2C address of light intensity sensor
 CONTINUOUS_HIGH_RES_MODE = 0x10 # start measuring at 1 lux res, measurement time ~120ms
 ONE_TIME_HIGH_RES_MODE = 0x20
+
+# SPI soil moisture variables
+spi_bus = 0
+spi_chip_select = 0
 
 
 # --- DEVICE FUNCTIONS ---
 def configure_devices():
 	dhtSensor = DHT.sensor(pigpio.pi(), DHT_PIN, model=DHT.DHTXX) 
 	bus = smbus.SMBus(I2C_CHANNEL)
+	spi = spidev.SpiDev()
+	spi.open(spi_bus,  spi_chip_select)
+	spi.mode = 0
+	spi.max_speed_hz = 500000
 	time.sleep(0.5) 
-	return dhtSensor, bus
+	return dhtSensor, bus, spi
 
 def read_temp_humidity(sensor):
 	timestamp, gpio, status, temperature_c, humidity = sensor.read()
@@ -30,15 +39,21 @@ def read_light_intensity(i2c_bus):
 	time.sleep(1) # need at least 120ms to take measurement
 	raw = i2c_bus.read_word_data(I2C_ADDRESS,0x00) # read data
 	# bytes in measurement will be swapped when read in 
-	light_intensity = ((raw & 0xff00) >> 8) | ((raw & 0x00ff) << 8) 
+	light_intensity = ((raw & 0xff00) >> 8) | ((raw & 0x00ff) << 8)
 	return light_intensity
+
+def read_soil_moisture(spi):
+	raw = spi.readbytes(2)
+	return ((raw[0]<<8)|raw[1])
 
 # --- NETWORK FUNCTIONS ---
 
 if __name__ == "__main__":
-	dhtSensor, i2c_bus = configure_devices()
-	t, h = read_temp_humidity(dhtSensor)
-	lx = read_light_intensity(i2c_bus)
-	print(f"Temperature:{t}\nHumidity:{h}\nLight Intensity:{lx}")
-
+	dhtSensor, i2c_bus, spi = configure_devices()
 	
+	while True:
+		t, h = read_temp_humidity(dhtSensor)
+		lx = read_light_intensity(i2c_bus)
+		soil_moisture = read_soil_moisture(spi)
+		print(f"Temperature:{t}\nHumidity:{h}\nLight Intensity:{lx}\nSoil Moisture:{soil_moisture}\n")
+		time.sleep(3)
